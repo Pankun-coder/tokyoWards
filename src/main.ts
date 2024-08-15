e3276a7eb02253d2a70262c9cdde47a54a4c0143
pankun-coder <jjohese@gmail.com>
2024-08-15 14:10:19 +0900

    refactor: use class

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,72 @@
 import { WardId, wardIdToName } from "./constants";
 
-import { onFormSubmit, onClickWard } from "./functions";
+import Game from "./game";
+
+const game = new Game();
+
+let currentWard: undefined | WardId;
+
+const getWard = (id: WardId) => {
+  const res = document.getElementById(id);
+  if (!res) {
+    throw new Error("ward not found");
+  }
+  return res;
+};
+
+const selectWard = (id: WardId) => {
+  const element = getWard(id);
+  currentWard = id;
+  element.style.fill = "red";
+};
+
+const unselectWard = (id: WardId) => {
+  const previous = getWard(id);
+  currentWard = undefined;
+  previous.style.fill = "#EEEEEE";
+};
 
 for (const ward in wardIdToName) {
-  const element = document.getElementById(ward);
-  if (!element) {
-    console.error(ward);
-    throw new Error("Ward not found");
-  }
-  element.addEventListener("click", () => onClickWard(ward as WardId));
+  const element = getWard(ward as WardId);
+  element.addEventListener("click", () => {
+    if (currentWard) {
+      unselectWard(currentWard);
+    }
+    selectWard(ward as WardId);
+  });
 }
 
 const form = document.getElementById("form") as HTMLFormElement | undefined;
 if (!form) {
   throw new Error("no form");
 }
-form.addEventListener("submit", onFormSubmit);
+const inputElement = document.getElementById("input") as
+  | HTMLInputElement
+  | undefined;
+if (!inputElement) {
+  throw new Error("Input not found");
+}
+
+form.addEventListener("submit", (event: SubmitEvent) => {
+  event.preventDefault();
+
+  const userInput = inputElement.value;
+  if (currentWard && game.isCorrectName(currentWard, userInput)) {
+    game.markAsDone(currentWard);
+    inputElement.value = "";
+    const element = getWard(currentWard);
+    element.style.fill = "green";
+    currentWard = undefined;
+    console.log("sesikai");
+    if (game.hasCompleted()) {
+      alert("clear!");
+      return;
+    }
+    const newWardId = game.getNotDoneWardRandomly();
+    selectWard(newWardId);
+  } else {
+    console.log("hazure");
+  }
+});
 
 // TODO: 名前の表示はできたらsvgの上がいいけど、難しそうなので別の枠にリストみたいに並べて行ってもいいかも
